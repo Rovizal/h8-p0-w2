165fbe5de10e46a22eff7aee4010c35182180ab5
Rovizal <vizalji@gmail.com>
2018-02-05 23:25:36 +0700

    tugas keenam

---
+++ exercise6.js
@@ -0,0 +1,75 @@
+//NUMBER 1
+var loop = 2;
+
+console.log('LOOPING PERTAMA');
+
+while (loop < 20) {loop+=2;
+  console.log(loop + ' - I LOVE CODING');
+}
+
+var loop = 22;
+
+console.log('LOOPING KEDUA');
+
+while (loop > 2) {loop -=2; console.log(loop + ' - I WILL BECOME FULLSTACK DEVELOPER')}
+
+//NUMBER 2
+console.log('LOOPING PERTAMA');
+
+for(var angka = 1; angka < 21; angka ++) {console.log(angka + ' - I love Coding');}
+
+console.log('LOOPING PERTAMA');
+
+for(var angka = 20; angka > 0; angka --) {console.log(angka + ' - I will become full stack developer');}
+
+//NUMBER 3
+for (var v = 1; v <= 100 ; v ++)
+
+
+  if(v % 2 === 0){
+
+    console.log ( v + ' - GENAP')
+
+  } else if (v % 2 !== 0 ){
+
+    console.log (v + ' - GANJIL')
+  }
+
+/////////
+for (var v = 1; v <=100; v += 2){
+
+
+ if (v % 3 === 0){
+
+   console.log (v + ' KELIPATAN ' + 3)
+
+ } else {
+
+   console.log(v + " - ")
+ }
+
+}
+///////
+for (var i = 1; i <=100; i+= 5){
+
+   if (i % 6 === 0){
+
+   console.log (i + ' KELIPATAN ' + 6)
+
+ } else {
+
+   console.log(i + " - ")
+ }
+
+}
+////////
+for (var i = 1; i <=100; i+= 9){
+
+ if (i % 10 === 0){
+
+   console.log (i + ' KELIPATAN ' + 10)
+
+ } else {
+
+   console.log(i + " - ")
+ }}
