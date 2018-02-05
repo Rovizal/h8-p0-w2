767553454f977bdbc5f1c378a4d81bde8e68c7cf
Rovizal <vizalji@gmail.com>
2018-02-05 12:35:23 +0700

    tugas ketiga

---
+++ exercise3.js
@@ -0,0 +1,12 @@
+var nama = '';
+var peran = '';
+
+if (nama === '' && peran ===''){
+  console.log("Nama tidak boleh kosong. Pilih peranmu untuk  memulai game");
+} else if (nama !== '' && peran === '') {console.log("pilih peranmu untuk memulai game");
+} else if (peran !== '' && nama === ''){
+  console.log('Nama tidak boleh kosong');
+} else if (nama !== '' && peran === 'Ksatria') {console.log("selamat datang di dunia Proxytia, "+nama+" . Halo Ksatria " +nama+ ", kamu dapat menyerang dengan senjatamu");
+} else if (nama !== '' && peran === 'Tabib') {console.log("selamat datang di dunia Proxytia, "+nama+" . Halo Tabib "+nama+", kamu dapat menyembuhkan temanmu yang terluka");
+} else if (nama !== '' && peran === 'Penyihir') {console.log("selamat datang di dunia Proxytia, "+nama+". Halo Penyihir "+nama+", ciptakan keajaiban yang membantu kemenanganmu");
+}
