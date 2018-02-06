a584241fbb8b317b1f87c50bed9092e0f3706711
Rovizal <vizalji@gmail.com>
2018-02-06 09:16:42 +0700

    tugas ketujuh

---
+++ exercise7.js
@@ -0,0 +1,20 @@
+//NUMBER 1
+console.log('rows1');
+for (var rows1 = 1; rows1 <= 5; rows1++) {console.log('*');}
+
+//NUMBER 2
+var hotelbintang5 = "*****";
+
+	 for (var i = 0; i < 6; i++) {
+	 	for(var v = 0; v < i; v++); console.log(hotelbintang5);}
+
+//NUMBER 3
+var a;
+var b;
+
+		for( a=10; a>0; a--){
+			for( b=10; b>a; b--){
+				console.log(" * ");
+			}
+			console.log(" ");
+		}
